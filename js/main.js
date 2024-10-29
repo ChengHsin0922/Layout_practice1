066e0de00217369565309702e84437a99a1fde7a
Breezy0922 <168520748+ChengHsin0922@users.noreply.github.com>
2024-10-29 19:46:35 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(function() {
+    console.log('ok');
+    // artist 
+    var swiper = new Swiper(".mySwiper", {
+        effect: "coverflow",
+        loop: true,
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 3,
+        initialSlide: 2,
+        coverflowEffect: {
+            rotate: 40, 
+            stretch: 0,
+            depth: 200,
+            modifier: 1,
+            slideShadows: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+        },
+    });
+    // masonry卡片
+    $('.grid').masonry({
+        // set itemSelector so .grid-sizer is not used in layout
+        itemSelector: '.grid-item',
+        // use element for option
+        columnWidth: '.grid-sizer',
+        percentPosition: true,
+    });
+    // 藝術家分頁TAB
+    $('.nav-link').on('click', function () {
+        $('.nav-link').removeClass('active');
+        $(this).addClass('active');
+        //獲取當前目標的data-target
+        const targetId = $(this).data('target');
+        //比對後切換目標
+        $('.content').each(function () {
+            $(this).toggleClass('hidden', $(this).attr('id') !== targetId);
+        });
+    })
+})
